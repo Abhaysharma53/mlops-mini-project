efc55fcae9dea13e8dcc194150733cedd6793c86
Abhay <abhaysharma.cs2017.pgdav@gmail.com>
2024-11-29 21:49:39 +0530

    modifying test file again

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,6 @@ import unittest
 import mlflow
 import os
 import pandas as pd
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pickle
 
 class TestModelLoading(unittest.TestCase):
@@ -24,27 +23,23 @@ class TestModelLoading(unittest.TestCase):
         # Set up MLflow tracking URI
         mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
-        # Load the new model from MLflow model registry
-        cls.new_model_name = "my_model"
-        cls.new_model_version = cls.get_latest_model_version(cls.new_model_name)
-        cls.new_model_uri = f'models:/{cls.new_model_name}/{cls.new_model_version}'
-        cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
+        # Load the model from MLflow model registry
+        cls.model_name = "my_model"
+        cls.model_version = cls.get_latest_model_version(cls.model_name)
+        cls.model_uri = f'models:/{cls.model_name}/{cls.model_version}'
+        cls.model = mlflow.pyfunc.load_model(cls.model_uri)
 
         # Load the vectorizer
         cls.vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
 
-        # Load holdout test data
-        cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
-
     @staticmethod
-    def get_latest_model_version(model_name, stage="Staging"):
+    def get_latest_model_version(model_name):
         client = mlflow.MlflowClient()
-        latest_version = client.get_latest_versions(model_name, stages=[stage])
+        latest_version = client.get_latest_versions(model_name, stages=["Staging"])
         return latest_version[0].version if latest_version else None
 
     def test_model_loaded_properly(self):
-        self.assertIsNotNone(self.new_model)
-
+        self.assertIsNotNone(self.model)
 
     def test_model_signature(self):
         # Create a dummy input for the model based on expected input shape
